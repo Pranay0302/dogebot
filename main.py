71928d1bd7c9715328f0f686566859c0428f2d1d
sonu ishaq <sonuishaq67@gmail.com>
2021-01-31 11:13:05 +0530

    removed unreachable code

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 import dogerep
 
-rt = dogerep.repeat_interval()
-while rt:
-    try:
-        rt
-        print('success')
-    except Exception as error:
-        print(error)
-        exit(0)
+try :
+    dogerep.repeat_interval()
+except Exception as err:
+    print(err)
