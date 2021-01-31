bc2b836c6d36df927c89203be14a4923a85ae417
sonu ishaq <sonuishaq67@gmail.com>
2021-01-31 12:05:42 +0530

    terminal output friendly

--- dogerep.py
+++ dogerep.py
@@ -1,19 +1,24 @@
 import requests
-import time, os
+import time, os,sys
 
 url = "https://api.wazirx.com/api/v2/tickers"
 
 
 def get_curr():
+    i=10
     res = requests.get(url, allow_redirects=True)
-    print(res.status_code)
     r = res.json()
     data = r["dogeinr"]["last"]
     os.system(f'echo `date "+%T"` {data} >> list.txt')
-
-
+    while True:
+        i=i-1
+        sys.stdout.write(f'\rCurrent rate in inr : ₹{data}  {i}s till refresh  ')
+        time.sleep(1)
+        sys.stdout.flush()
+        if i==0:
+            break
+    
 def repeat_interval():
     get_curr()
-    time.sleep(10)
     repeat_interval()
 
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import dogerep
+import dogerep,os
 
 try :
+    os.system("clear")
     dogerep.repeat_interval()
 except Exception as err:
     print(err)
