e0bbda51332509559d29c42202f3c4211738c00c
sonu ishaq <sonuishaq67@gmail.com>
2021-01-31 10:52:13 +0530

    cleaned the code

--- dogerep.py
+++ dogerep.py
@@ -1,10 +1,22 @@
 import requests
+import time, os
 from decouple import config
 
-url = config('URL')
-res = requests.get(url)
+url = config("URL")
 
-print(res.status_code)
-r = res.json()
-data = r['dogeinr']
-print(data)
+
+def get_curr():
+    res = requests.get(url, allow_redirects=True)
+    print(res.status_code)
+    r = res.json()
+    data = r["dogeinr"]["last"]
+    os.system(f'echo `date "+%T"` {data} >> list')
+
+
+def repeat_interval():
+    get_curr()
+    time.sleep(10)
+    repeat_interval()
+
+
+repeat_interval()
