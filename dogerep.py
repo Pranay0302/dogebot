f2c22540bd51c3fe6d92202a92f0bfc6c7f65ef6
sonu ishaq <sonuishaq67@gmail.com>
2021-02-01 13:16:16 +0530

    moved to curses

--- dogerep.py
+++ dogerep.py
@@ -1,11 +1,11 @@
-import requests, time, os, sys, json
+import requests, time, os, sys, json, curses
 
 url = "https://api.wazirx.com/api/v2/tickers"
 
 data = {}
 
-with open("./data.json") as f:
-    data = json.load(f)
+with open("./data.json", "r") as f:
+    data = json.loads(f.read())
 
 
 def get_curr():
@@ -13,29 +13,57 @@ def get_curr():
     res = requests.get(url, allow_redirects=True)
     r = res.json()
     value = r["dogeinr"]["last"]
+    check_and_play(float(value))
     os.system(f'echo `date "+%T"` {value} >> list.txt')
     while True:
         i = i - 1
         amount = float(value) * data["total-coins"]
-        sys.stdout.write(
-            f"\n\t\tCurrent rate in inr : ₹{value} \n\t\tTotal Balance left is {amount}"
-        )
+        stdscr.addstr(0,0,f"\n\t\tCurrent rate in inr : ₹{value} \n\t\tTotal Balance left is {amount}")
         people = data["people"]
+        ii=6
         for one in people:
-            sys.stdout.write(
-                f"\n\t\t{one['name']} Balance left is {float(value)*one['coins']}"
-            )
-        sys.stdout.write(f"\n\t\t{i}s till refresh\n")
-        time.sleep(1)
-        os.system("clear")
+            stdscr.addstr(ii,0,f"\t\t{one['name']} Balance left is {float(value)*one['coins']}")
+            ii=ii+1
+        stdscr.addstr(ii+1,0,f"\n\t\t{i}s till refresh\n")
+        stdscr.refresh()
+        try:
+            time.sleep(1)
+        except:
+            print("\n bye bye")
+            exit(0)
         if i == 0:
             break
+
+
+def check_and_play(value):
+    if float(value) >= 3:
+        os.system("mpg123 ./siren.mp3 &")
 
 
 def repeat_interval():
     try:
         get_curr()
-    except:
+    except Exception as ex:
+        # print(ex)
         print("\n bye bye")
         exit(0)
-    repeat_interval()
+    try:
+        repeat_interval()
+    except:
+        pass
+
+if __name__ == "__main__":
+    stdscr = curses.initscr()
+    curses.noecho()
+    curses.cbreak()
+
+    try:
+        repeat_interval()
+    except Exception as ex:
+        # print(ex)
+        print("\n bye bye")
+        exit(0)
+    finally:
+        curses.echo()
+        curses.nocbreak()
+        curses.endwin()
--- main.py
+++
@@ -1,7 +0,0 @@
-import dogerep,os
-
-try :
-    os.system("clear")
-    dogerep.repeat_interval()
-except Exception as err:
-    print(err)
