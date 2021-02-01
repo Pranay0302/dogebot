d5e6143457cfc248614609e57a813a8eef7f99a7
sonu ishaq <sonuishaq67@gmail.com>
2021-02-01 10:32:42 +0530

    json data and exit

--- dogerep.py
+++ dogerep.py
@@ -1,24 +1,41 @@
-import requests
-import time, os,sys
+import requests, time, os, sys, json
 
 url = "https://api.wazirx.com/api/v2/tickers"
 
+data = {}
+
+with open("./data.json") as f:
+    data = json.load(f)
+
 
 def get_curr():
-    i=10
+    i = 10
     res = requests.get(url, allow_redirects=True)
     r = res.json()
-    data = r["dogeinr"]["last"]
-    os.system(f'echo `date "+%T"` {data} >> list.txt')
+    value = r["dogeinr"]["last"]
+    os.system(f'echo `date "+%T"` {value} >> list.txt')
     while True:
-        i=i-1
-        sys.stdout.write(f'\rCurrent rate in inr : ₹{data}  {i}s till refresh  ')
+        i = i - 1
+        amount = float(value) * data["total-coins"]
+        sys.stdout.write(
+            f"\n\t\tCurrent rate in inr : ₹{value} \n\t\tTotal Balance left is {amount}"
+        )
+        people = data["people"]
+        for one in people:
+            sys.stdout.write(
+                f"\n\t\t{one['name']} Balance left is {float(value)*one['coins']}"
+            )
+        sys.stdout.write(f"\n\t\t{i}s till refresh\n")
         time.sleep(1)
-        sys.stdout.flush()
-        if i==0:
+        os.system("clear")
+        if i == 0:
             break
-    
-def repeat_interval():
-    get_curr()
-    repeat_interval()
 
+
+def repeat_interval():
+    try:
+        get_curr()
+    except:
+        print("\n bye bye")
+        exit(0)
+    repeat_interval()
